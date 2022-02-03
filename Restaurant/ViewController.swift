1bd516ed44c777644c94c10a07b9c12bfe64910e
Edgar <engineeredkar@gmail.com>
2022-02-03 15:42:00 +0400

    Initial Commit

---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Edgar Karapetyan on 03.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
