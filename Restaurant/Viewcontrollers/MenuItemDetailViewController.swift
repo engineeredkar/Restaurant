17de1c664413360f3ec35df2f2493adacaa4506c
Edgar <engineeredkar@gmail.com>
2022-02-20 21:20:46 +0400

    implemented MenuTableViewController  and MenuItemDetailViewController classes

--- Restaurant/Viewcontrollers/MenuItemDetailViewController.swift
+++ Restaurant/Viewcontrollers/MenuItemDetailViewController.swift
@@ -8,8 +8,43 @@
 import UIKit
 
 class MenuItemDetailViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var detailTextLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var addToOrderButton: UIButton!
+    
+    let menuItem: MenuItem
+    
+    init?(coder: NSCoder, menuItem: MenuItem) {
+        self.menuItem = menuItem
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
+    }
+    
+    func updateUI() {
+        nameLabel.text = menuItem.name
+        priceLabel.text = menuItem.price.formatted(.currency(code: "usd"))
+        detailTextLabel.text = menuItem.detailText
+    }
+    
+    @IBAction func orderButtonTapped(_ sender: Any) {
+        UIView.animate(withDuration: 0.5, delay: 0,
+                       usingSpringWithDamping: 0.7, initialSpringVelocity: 0.1,
+                       options: [], animations: {
+            self.addToOrderButton.transform =
+            CGAffineTransform(scaleX: 2.0, y: 2.0)
+            self.addToOrderButton.transform =
+            CGAffineTransform(scaleX: 1.0, y: 1.0)
+        }, completion: nil)
     }
 }
